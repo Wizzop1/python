2b92561e0ff3d07e65db8de9114f588b2a24f165
Wizzop1 <75120002+Wizzop1@users.noreply.github.com>
2022-10-21 11:21:17 +0300

    functions

---
+++ func2.py
@@ -0,0 +1,15 @@
+def math(a,m_op,b):
+    a=int(a)
+    b=int(b)
+    match m_op:
+        case '+':
+            print(a+b)
+        case '-':
+            print(a-b)
+        case '*':
+            print(a*b)
+        case '/':
+            print(a/b)
+
+math(input("First number: "),input("Operation: "),input("Second number: "))
+
---
+++ matchcase.py
@@ -0,0 +1,6 @@
+ex= input()
+match ex:
+    case '1':
+        "You wrote 1"
+    case '2':
+        'You wrote 2'
